0da1af077f12fe3d5a17136018840ace7ffc31f9
dongjiajia <jiajiadong11@gmail.com>
2019-11-23 16:37:35 +0800

    学习

---
+++ app/src/main/java/com/example/timetablemaster/interfaces/BClass.java
@@ -0,0 +1,10 @@
+package com.example.timetablemaster.interfaces;
+
+public class BClass extends AbClass {
+
+    @Override
+    public void Out() {
+        super.Out();
+        System.out.print("This is B class!");
+    }
+}
